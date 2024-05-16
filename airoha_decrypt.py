3c67eb3be64be54d180daf72c96e83bd445210c6
mos9527 <greats3an@gmail.com>
2024-05-16 22:09:15 +0800

    airoha_decrypt.py : Fixed AES decryption routine. airoha_decrypt.py : Made commandline arguments `from` `to` positional arguments. airoha_decrypt.py : Added option to skip decompression. airoha_decrypt.py : Added sample files for testing. README.md : Updated README with new usage instructions.

--- airoha_decrypt.py
+++ airoha_decrypt.py
@@ -7,9 +7,6 @@ import struct
 from Crypto.Cipher import AES
 
 
-ENCRYPTED_BLOCK_SIZE = 256
-
-
 class AirohaDecryptInputAndOutputFilesMustBeDifferent(Exception):
     pass
 
@@ -17,37 +14,30 @@ class AirohaDecryptInputAndOutputFilesMustBeDifferent(Exception):
 def _parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--key', type=binascii.unhexlify, required=True,
-                        help='AES-128 key. Hex format.')
-    parser.add_argument('--iv', type=binascii.unhexlify, required=True,
-                        help='A 16-bytes IV for the AES-CBC. Hex format.')
-    parser.add_argument('--from', dest='_from', type=argparse.FileType('rb'), required=True,
-                        help='Encrypted firmware file.')
-    parser.add_argument('--to', required=True,
-                        help='Decrypted firmware file.')
-    parser.add_argument('--offset', type=lambda x: int(x, 0), default=0,
+    parser.add_argument('_from', type=argparse.FileType('rb'), help='Encrypted firmware file.')
+    parser.add_argument('to', help='Decrypted (Decompressed too if applicable) file section.')
+    
+    parser.add_argument('--offset', type=lambda x: int(x, 0), default='0x1000',
                         help='Offset of the encrypted part in the input file.')
-    parser.add_argument('--verify-lzma', action='store_true',
-                        help='Verify that the result is a valid LZMA file.')
-
+    parser.add_argument('--key', type=binascii.unhexlify, default='000102030405060708090a0b0c0d0e0f',
+                        help='AES-128 key. Hex format.')
+    parser.add_argument('--iv', type=binascii.unhexlify, default='62633636633839306334636432383763',
+                        help='A 16-bytes IV for the AES-CBC. Hex format.')
+    parser.add_argument('--no-decompress', action='store_true', help='Do not decompress the decrypted data.')
     return parser.parse_args()
 
 
 def _decrypt(ciphertext, key, iv, offset=0):
     ciphertext = ciphertext[offset:]
-
-    decrypted = b''
-    for index in range(0, len(ciphertext), ENCRYPTED_BLOCK_SIZE):
-        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-        decrypted += cipher.decrypt(ciphertext[index: index+ENCRYPTED_BLOCK_SIZE])
-
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    decrypted = cipher.decrypt(ciphertext) # NOTE: Data may come with padding (with bytes 0xFF)    
     return decrypted
 
 
-def _verify_lzma_file(decrypted_data):
+def _decompress(decrypted_data):
     # CPython can't handle an initialized size field in the LZMA header, so we set it to -1
     fixed_lzma = decrypted_data[:5] + struct.pack('<Q', 0xFFFFFFFFFFFFFFFF) + decrypted_data[5+8:]
-    lzma.decompress(fixed_lzma, format=lzma.FORMAT_ALONE)
+    return lzma.decompress(fixed_lzma, format=lzma.FORMAT_ALONE)
 
 
 if __name__ == '__main__':
@@ -58,10 +48,18 @@ if __name__ == '__main__':
 
     with args._from as encrypted_file:
         encrypted_data = encrypted_file.read()
+    
+    args.key = args.key[::-1]
+    args.iv = args.iv[::-1]
 
-    decrypted_data = _decrypt(encrypted_data, args.key, args.iv, args.offset)
-    if args.verify_lzma:
-        _verify_lzma_file(decrypted_data)
+    result = _decrypt(encrypted_data, args.key, args.iv, args.offset)
 
-    with open(args.to, 'wb') as decrypted_file:
-        decrypted_file.write(decrypted_data)
+    if not args.no_decompress:
+        try:
+            decompressed_data = _decompress(result)
+            result = decompressed_data
+        except lzma.LZMAError as e:
+            print(f'Failed to decompress: {e}. Writing decrypted data only.')
+
+    with open(args.to, 'wb') as output:
+        output.write(result)
